ea2bb882da119d4f884129d3a26a10e88e21ce8b
JosiahSco <jscott70@uco.edu>
2023-11-18 20:14:56 -0600

    fix: linking database

---
+++ app/api/get-words/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import { get } from '@vercel/edge-config';
+
+export async function POST(request) {
+    let numWords = await request.json();
+
+    const allWords = await get('words');
+    let selectedWordSet = new Set();
+    while (selectedWordSet.size < numWords) {
+        selectedWordSet.add(allWords[Math.floor(Math.random() * allWords.length)]);
+    }
+
+    return NextResponse.json(Array.from(selectedWordSet));
+}
